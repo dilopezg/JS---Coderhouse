aae61cd4eb618ca3ef8baf9342b7f38b56cbbeeb
dilopezg <dilialopezg@gmail.com>
2023-02-01 23:39:55 -0500

    challenge 18 primer commit

---
+++ Challenge_18/logger/logger.js
@@ -0,0 +1,14 @@
+import winston from 'winston'
+import { createLogger} from 'winston'
+
+const logger = createLogger({
+        level: "info",
+        transports: [
+            new winston.transports.Console({ level: "info", }),
+            new winston.transports.File({filename: "./logger/warn.log",level: "warn",}),
+            new winston.transports.File({filename: "./logger/error.log",level: "error",})
+        ],
+    });
+
+
+export default  logger;
